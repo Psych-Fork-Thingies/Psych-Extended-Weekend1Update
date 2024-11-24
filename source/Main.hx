cb96928babb85e57155d145fd19fb0d98d205bd7
28AloneDark53 <28kraloyuncu28@gmail.com>
2024-11-24 23:56:18 +0300

    classic 3

--- source/Main.hx
+++ source/Main.hx
@@ -165,7 +165,7 @@ class Main extends Sprite
 				fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
 		     if (FlxG.cameras != null) {
 			   for (cam in FlxG.cameras.list) {
-				if (cam != null)
+				if (cam != null && cam.filters != null)
 					resetSpriteCache(cam.flashSprite);
 			   }
 			}
