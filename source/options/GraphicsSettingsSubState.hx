7f400fcb70a455237560aaa2c50f7898f078253c
Prohack101010 <miracberaber@gmail.com>
2025-01-03 20:52:47 +0300

    Oops

--- source/options/GraphicsSettingsSubState.hx
+++ source/options/GraphicsSettingsSubState.hx
@@ -52,13 +52,13 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 		addOption(option);
 
 		var option:Option = new Option('Shaders', //Name
-			'If unchecked, disables shaders.\nIt's used for some visual effects, and also CPU intensive for weaker $Main.platform' + ".", //Description
+			"If unchecked, disables shaders.\nIt's used for some visual effects, and also CPU intensive for weaker $Main.platform" + ".", //Description
 			'shaders', //Save data variable name
 			'bool');
 		addOption(option);
 		
 		var option:Option = new Option('Loading Screens', //Name
-			'EXPERIMENTAL: Allows Loading Screen, speeds up loading times,\nbut might be incompatible with older $Main.platform' + ".", //Description
+			"EXPERIMENTAL: Allows Loading Screen, speeds up loading times,\nbut might be incompatible with older $Main.platform" + ".", //Description
 			'loadingScreen',
 			'bool');
 		addOption(option);
