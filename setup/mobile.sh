e207b5a31a618c81b04d48e3be0a1df485cf97ec
Prohack101010 <miracberaber@gmail.com>
2025-03-25 21:07:54 +0300

    Very Silly Me

--- setup/mobile.sh
+++ setup/mobile.sh
@@ -17,7 +17,7 @@ haxelib install hscript 2.4.0 --quiet
 haxelib git hxCodec https://github.com/MobilePorting/hxCodec-0.6.3 --quiet
 haxelib git hxcpp https://github.com/mcagabe19-stuff/hxcpp --quiet
 haxelib git lime https://github.com/MobilePorting/lime --quiet
-haxelib install openfl 9.3.3 --quiet
+haxelib git openfl https://github.com/MobilePorting/openfl 9.3.3 --quiet
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate.git dev --quiet
 haxelib git funkin.vis https://github.com/beihu235/funkVis-FrequencyFixed main --quiet
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git 57f5d47f2533fd0c3dcd025a86cb86c0dfa0b6d2 --quiet
