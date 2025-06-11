86da55d2dca10a2151553e016d527c9a31b21d3d
The Man Daffyo Himself <87585374+DaffyToons@users.noreply.github.com>
2025-06-10 23:00:39 -0400

    Update MainMenuStateOld.hx

--- source/extras/states/MainMenuStateOld.hx
+++ source/extras/states/MainMenuStateOld.hx
@@ -62,7 +62,6 @@ class MainMenuStateOld extends MusicBeatState
 		#if MODS_ALLOWED
 		Mods.pushGlobalMods();
 		#end
-		Mods.loadTopMod();
 
 		#if desktop
 		// Updating Discord Rich Presence
@@ -94,14 +93,13 @@ class MainMenuStateOld extends MusicBeatState
 		add(camFollow);
 		add(camFollowPos);
 
-		magenta = new FlxSprite(-80).loadGraphic(Paths.image('menuDesat'));
+		magenta = new FlxSprite(-80).loadGraphic(Paths.image('menuBGMagenta'));
 		magenta.scrollFactor.set(0, yScroll);
 		magenta.setGraphicSize(Std.int(magenta.width * 1.175));
 		magenta.updateHitbox();
 		magenta.screenCenter();
 		magenta.visible = false;
 		magenta.antialiasing = ClientPrefs.data.antialiasing;
-		magenta.color = 0xFFfd719b;
 		add(magenta);
 
 		// magenta.scrollFactor.set();
